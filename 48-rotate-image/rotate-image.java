38b5bd98dae6f2ed1270ca0e58f719a916272a0c
PriyankaCh345 <priyankachaudhary09112004@gmail.com>
2026-06-27 13:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (97.49%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -6,7 +6,7 @@
 
         // Transpose
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+            for (int j = 0; j < i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
